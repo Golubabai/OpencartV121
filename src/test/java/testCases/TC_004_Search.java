41f3b5b49f40465ac34be771095bf4861e0bb231
Golubabai <basuajitava20@outlook.com>
2025-08-04 00:24:19 +0530

    Update TC_004_Search.java

--- src/test/java/testCases/TC_004_Search.java
+++ src/test/java/testCases/TC_004_Search.java
@@ -1,5 +1,34 @@
 package testCases;
 
 public class TC_004_Search {
-// implement later.
+
+	
+	    // Function to print N Fibonacci Number
+	    static void Fibonacci(int N)
+	    {
+	        int n1 = 0, n2 = 1;
+
+	        for (int i = 0; i < N; i++) {
+	            // Print the number
+	            System.out.print(n1 + " ");
+
+	            // Swap
+	            int n3 = n2 + n1;
+	            n1 = n2;
+	            n2 = n3;
+	        }
+	    }
+	
+
+	
+
+
+public static void main(String args[])
+{
+    // Given Number N
+    int N = 10;
+
+    // Function Call
+    Fibonacci(N);
+}
 }
